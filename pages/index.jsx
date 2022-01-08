4312843610ccfe101f43040b3bde3eca6dea4e34
clnhs <git@colinhs.com>
2022-01-08 17:00:40 -0500

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+    return (
+        <div className={``}>
+            <h1 className={`text-3xl`}>React-Jotoba</h1>
+        </div>
+    );
+};
+
+export default IndexPage;
