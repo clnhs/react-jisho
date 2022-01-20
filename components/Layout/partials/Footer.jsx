d16081a295f9fecb76c3926585d7551fb7452ec1
clnhs <git@colinhs.com>
2022-01-19 20:19:04 -0500

    ENHANCEMENT: smaller font on the footer.

--- components/Layout/partials/Footer.jsx
+++ components/Layout/partials/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer
-            className={`z-10 flex flex-col flex-nowrap shrink-0 w-full p-4 sm:p-1 justify-center items-center text-white bg-black dark:bg-gray-800`}
+            className={`z-10 flex flex-col flex-nowrap shrink-0 w-full text-xs py-0.5 sm:p-1 justify-center items-center text-white bg-black dark:bg-gray-800`}
         >
             <p>Built by @clnhs. Based on API from <a className={`font-bold underline`} href="https://jotoba.de">Jotoba.de</a>.</p>
         </footer>
