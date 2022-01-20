3b3ef6ab5ee2f2d584398c5f02b9fb9e40b31cea
clnhs <git@colinhs.com>
2022-01-19 20:19:37 -0500

    ENHANCEMENT: added Head. Mostly Apple-related stuff + title.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
+import Head from "next/head";
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({Component, pageProps}) => {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    );
+        <>
+            <Head>
+                <title>React辞書</title>
+                <link rel="icon" type="image/x-icon" ref="/favico.ico"/>
+                <link rel="icon" type="image/png" href="/favico.png"/>
+                <link rel="apple-touch-icon" href="/favico-apple.png"/>
+                <link rel="apple-touch-startup-image" href="/launch.png"/>
+                <meta name="apple-mobile-web-app-title" content="React辞書"/>
+                <meta name="apple-mobile-web-app-capable" content="yes"/>
+                <link rel="manifest" href="/manifest.json"/>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout></>
+);
 };
 
 export default MyApp;
