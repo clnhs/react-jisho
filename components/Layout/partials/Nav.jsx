53bc8607841f18459cd435d3f4090290e9dafc53
clnhs <git@colinhs.com>
2022-01-19 20:15:27 -0500

    Removed some cruft and fixed positioning issues on smaller display of the nav bar.

--- components/Layout/partials/Nav.jsx
+++ components/Layout/partials/Nav.jsx
@@ -1,11 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {
-    MdHelpOutline,
-    MdNotifications,
-    MdSettings,
-} from "react-icons/md";
+import { MdHelpOutline } from "react-icons/md";
 import SearchBar from "../../SearchBar/SearchBar";
 import { useRouter } from "next/router";
 
@@ -13,13 +9,9 @@ const Nav = props => {
     const router = useRouter();
     const isOnHome = router.pathname === "/";
 
-    useEffect(() => {
-        console.log();
-    }, [router]);
-
     return (
         <nav
-            className={`grid grid-cols-12 sticky top-0 z-10 justify-between w-full h-16 box-content ${
+            className={`grid grid-cols-4 md:grid-cols-12 sticky top-0 z-10 justify-between w-full h-16 box-content ${
                 !isOnHome
                     ? "bg-gray-100 dark:bg-gray-700 dark:backdrop-blur-2xl dark:bg-opacity-70 border-b border-black/10 dark:border-gray-800 dark:drop-shadow-lg"
                     : ""
@@ -29,7 +21,7 @@ const Nav = props => {
                 <>
                     <div
                         id={"nav-group-left"}
-                        className={`hidden sm:flex pl-4 flex-row flex-nowrap col-span-2 justify-start items-center text-2xl`}
+                        className={`col-span-1 md:col-span-2 hidden sm:flex flex-row flex-nowrap justify-start items-center pl-2 text-2xl`}
                     >
                         <Link href={"/"}>
                             <a className={`inline-flex`}>
@@ -44,7 +36,7 @@ const Nav = props => {
                     </div>
                     <div
                         id={"nav-group-center"}
-                        className={`flex flex-row flex-nowrap col-span-8 col-start-3 justify-center items-center`}
+                        className={`col-span-3 sm:col-span-2 md:col-span-8 md:col-start-3 flex flex-row flex-nowrap justify-center items-center px-2`}
                     >
                         <SearchBar navbarEmbed />
                     </div>
@@ -52,15 +44,19 @@ const Nav = props => {
             )}
             <div
                 id={"nav-group-right"}
-                className={`flex flex-row flex-nowrap col-span-2 col-start-11 justify-end items-center text-2xl`}
+                className={`flex flex-row flex-nowrap col-span-1 ${
+                    isOnHome
+                        ? "col-start-4"
+                        : "col-start-auto"
+                } md:col-span-2 md:col-start-11 justify-end items-center text-2xl`}
             >
+                {/*<Link href="#">*/}
+                {/*    <a className={`py-2 px-2 md:px-4`}>*/}
+                {/*        <MdSettings />*/}
+                {/*    </a>*/}
+                {/*</Link>*/}
                 <Link href="#">
-                    <a className={`py-2 px-4`}>
-                        <MdSettings />
-                    </a>
-                </Link>
-                <Link href="#">
-                    <a className={`py-2 px-4`}>
+                    <a className={`py-2 px-2 md:px-4`}>
                         <MdHelpOutline />
                     </a>
                 </Link>
