317209537d58831d00e435c7bda5351803fb2e70
clnhs <git@colinhs.com>
2022-02-01 20:31:23 -0500

    fix: #12

--- utils/JotobaUtils.js
+++ utils/JotobaUtils.js
@@ -1,4 +1,30 @@
-export const parsePos = part => {
+import { simple as simplePosData, detailed as detailedPosData } from "./PartsOfSpeech.json";
+
+export const parsePos = (pos, short = false, kana = false, lang = "en") => {
+    let posStr = "missing";
+    let posMatch = null;
+    if (typeof pos === "object") {
+        const mainPart = Object.keys(pos)[0];
+        const subPart = Object.values(pos)[0];
+        if (subPart){
+            if (typeof subPart === "string")
+                posMatch = detailedPosData[mainPart][subPart];
+            else
+                posMatch = detailedPosData[mainPart][Object.keys(subPart)[0]].types[Object.values(subPart)[0]]
+        }
+        else posMatch = detailedPosData[mainPart];
+    } //string
+    else posMatch = simplePosData[pos];
+
+    if (posMatch) {
+        if (short) posStr = posMatch.short;
+        else posStr = posMatch.long || posMatch;
+    }
+
+    return posStr;
+};
+
+export const parsePos_old = part => {
     const partsOfSpeech = [];
 
     if (typeof part === "string") {
@@ -19,7 +45,7 @@ export const parsePos = part => {
             partsOfSpeech.push(`Noun`);
         else
             partsOfSpeech.push(
-                part.Noun.toLowerCase() + " noun"
+                part.Noun.toLowerCase() + " noun",
             );
     } else if (part.hasOwnProperty("Adjective"))
         if (part.Adjective === "PreNounVerb")
