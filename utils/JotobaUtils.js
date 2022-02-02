081b678887b4f9b5f5e6cc786c959034ddc294b1
clnhs <git@colinhs.com>
2022-02-01 22:09:46 -0500

    code: removed old parsePos function.

--- utils/JotobaUtils.js
+++ utils/JotobaUtils.js
@@ -23,35 +23,3 @@ export const parsePos = (pos, short = false, kana = false, lang = "en") => {
 
     return posStr;
 };
-
-export const parsePos_old = part => {
-    const partsOfSpeech = [];
-
-    if (typeof part === "string") {
-        if (part !== "AuxiliaryVerb") {
-            partsOfSpeech.push(`Auxiliary verb`);
-        } else partsOfSpeech.push(part);
-    } else if (part.hasOwnProperty("Verb")) {
-        if (typeof part.Verb === "string")
-            partsOfSpeech.push(part.Verb + " verb");
-        else if (part.Verb.hasOwnProperty("Godan")) {
-            partsOfSpeech.push(`Godan verb`);
-        } else if (part.Verb.hasOwnProperty("Irregular")) {
-            if (part.Verb.Irregular === "NounOrAuxSuru")
-                partsOfSpeech.push(`Irregular 〜する verb`);
-        }
-    } else if (part.hasOwnProperty("Noun")) {
-        if (part.Noun === "Normal")
-            partsOfSpeech.push(`Noun`);
-        else
-            partsOfSpeech.push(
-                part.Noun.toLowerCase() + " noun",
-            );
-    } else if (part.hasOwnProperty("Adjective"))
-        if (part.Adjective === "PreNounVerb")
-            partsOfSpeech.push(`Pre-noun/verb adj.`);
-        else partsOfSpeech.push(`${part.Adjective} adj.`);
-    else partsOfSpeech.push(JSON.stringify(part.Verb));
-
-    return partsOfSpeech;
-};
