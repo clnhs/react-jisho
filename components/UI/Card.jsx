29345a5f8badb3c45a2839e14fd3266fd7f204ed
clnhs <git@colinhs.com>
2022-02-04 16:26:50 -0500

    code: result cards now all use same base component.

---
+++ components/UI/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = (props) => {
+    const { onClick, className } = props || undefined;
+
+    return (
+        <div
+            className={`w-full box-content group first:mt-0 p-[1px] rounded-lg bg-black/10 dark:bg-black/40 hover:shadow-sm hover:shadow-blue-300 hover:bg-blue-300 hover:cursor-pointer transition-all` + className}
+            onClick={onClick}
+        >
+            <div className={`w-full h-full rounded-md border-2 border-transparent w-full bg-white dark:bg-gray-700 group-hover:bg-opacity-80 transition-all`}>
+                {props.children}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
